bb358798db5c92e602ebf478a837b221aa23a27b
Manoj <manoj0011989@gmail.com>
2014-11-24 19:48:06 +0530

    Updated tests

--- Interview/src/cracking/the/coding/interview/chapter9/Prob92.java
+++ Interview/src/cracking/the/coding/interview/chapter9/Prob92.java
@@ -2,27 +2,28 @@ package cracking.the.coding.interview.chapter9;
 
 public class Prob92 {
 	public static int solution(int x, int y) {
-		int[][] grid = new int[x][y];
+		int[][] grid = new int[x + 1][y + 1];
 		for (int i = 0; i < x; i++) {
 			for (int j = 0; j < y; j++) {
-				grid[i][j] = -1;
+				grid[i][j] = 0;
 			}
 		}
 		return numPaths(x, y, grid);
 	}
 	private static int numPaths(int x, int y, int[][] grid) {
-		if (x == 0) {
+		if ((x == 1) || (y == 1)) { // TODO : why 1??
 			return 1;
 		}
-		if (y == 0) {
-			return 1;
-		}
-		if (grid[x][y] > -1) {
+		if (grid[x][y] > 0) {
 			return grid[x][y];
 		} else {
 			grid[x][y] = numPaths(x - 1, y, grid) + numPaths(x, y - 1, grid);
 			return grid[x][y];
 		}
 
+	}
+
+	public static void main(String args[]) {
+		System.out.println(solution(3, 3));
 	}
 }
